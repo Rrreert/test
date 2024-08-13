a2eaab5de0266f525a8fce98ba4cef08987b9831
Rrreert <96475871+Rrreert@users.noreply.github.com>
2024-08-13 14:57:20 +0800

    Update streamlit_MDR.py

--- streamlit_MDR.py
+++ streamlit_MDR.py
@@ -27,7 +27,7 @@ if "model" not in st.session_state:
 loaded_model = st.session_state["model"]
 loaded_tokenizer = st.session_state["tokenizer"]
 
-test_txt = "I don‚Äôt understand how I‚Äôm feeling and all I can describe it as is numbness but it‚Äôs past that point and I‚Äôve felt like this for a long time, I feel like I don‚Äôt belong to this life like it isn‚Äôt for me. I can‚Äôt see myself in any career, my own family my own little life I can‚Äôt see it , I‚Äôm so disconnected from social interaction I don‚Äôt leave my house much and the sad thing is as much as I hate it I don‚Äôt want to change it I have no motivation I‚Äôm so tired to the point I don‚Äôt see a point on living when I‚Äôm so tired I can‚Äôt do daily life like everyone. What is the point to this life? How do you really find happiness I feel nothing I get the occasional anger and I‚Äôm always Irritated but besides that I feel nothing and I hate it I can‚Äôt cry I can‚Äôt laugh I can‚Äôt feel anything"
+test_txt = st.text_area("请输入文本", None)
 if st.button('predict'):
     with st.spinner('model load...'):
         # Tokenize新数据
