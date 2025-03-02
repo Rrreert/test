f7d77ecea985e9f890ed4ecc6c1892c13b5f145b
Rrreert <96475871+Rrreert@users.noreply.github.com>
2025-03-02 14:15:55 +0800

    Delete e-commerce.py

--- e-commerce.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-from modelscope.pipelines import pipeline
-from modelscope.utils.constant import Tasks
-from modelscope.outputs import OutputKeys
-
-img_captioning = pipeline(Tasks.image_captioning, model='damo/ofa_image-caption_muge_base_zh', model_revision='v1.0.1')
-ner_pipeline = pipeline(Tasks.named_entity_recognition, 'damo/nlp_raner_named-entity-recognition_chinese-base-ecom-50cls')
-
-st.button("xxx")
